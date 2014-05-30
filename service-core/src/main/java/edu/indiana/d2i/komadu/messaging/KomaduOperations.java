2562452479022b979818cfc0adc68cd747d567b2
Isuru Suriarachchi <isurues@gmail.com>
2014-05-30 16:22:28 -0400

    Adding new operations into messaging api and adding samples related to those operations

--- service-core/src/main/java/edu/indiana/d2i/komadu/messaging/KomaduOperations.java
+++ service-core/src/main/java/edu/indiana/d2i/komadu/messaging/KomaduOperations.java
@@ -1,5 +1,6 @@
 package edu.indiana.d2i.komadu.messaging;
 
+import edu.indiana.d2i.komadu.query.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.xmlbeans.XmlException;
@@ -9,18 +10,6 @@ import org.apache.xmlbeans.XmlOptions;
 import edu.indiana.d2i.komadu.ingest.IngestException;
 import edu.indiana.d2i.komadu.ingest.NotificationIngester;
 import edu.indiana.d2i.komadu.ingest.NotificationSummary;
-import edu.indiana.d2i.komadu.query.FindActivityRequestDocument;
-import edu.indiana.d2i.komadu.query.FindActivityResponseDocument;
-import edu.indiana.d2i.komadu.query.GetActivityGraphRequestDocument;
-import edu.indiana.d2i.komadu.query.GetActivityGraphResponseDocument;
-import edu.indiana.d2i.komadu.query.GetAgentGraphRequestDocument;
-import edu.indiana.d2i.komadu.query.GetAgentGraphResponseDocument;
-import edu.indiana.d2i.komadu.query.GetContextWorkflowGraphRequestDocument;
-import edu.indiana.d2i.komadu.query.GetContextWorkflowGraphResponseDocument;
-import edu.indiana.d2i.komadu.query.GetEntityGraphRequestDocument;
-import edu.indiana.d2i.komadu.query.GetEntityGraphResponseDocument;
-import edu.indiana.d2i.komadu.query.QueryException;
-import edu.indiana.d2i.komadu.query.QueryImplementer;
 import edu.indiana.d2i.komadu.query.QuerySummary.QueryTypeEnum;
 import edu.indiana.d2i.komadu.service.AddActivityActivityRelationshipDocument;
 import edu.indiana.d2i.komadu.service.AddActivityEntityRelationshipDocument;
@@ -29,136 +18,156 @@ import edu.indiana.d2i.komadu.service.AddAgentAgentRelationshipDocument;
 import edu.indiana.d2i.komadu.service.AddAgentEntityRelationshipDocument;
 import edu.indiana.d2i.komadu.service.AddAttributesDocument;
 import edu.indiana.d2i.komadu.service.AddEntityEntityRelationshipDocument;
+
 /**
  * @author Quan Zhou (quzhou@indiana.edu)
  */
 
 public class KomaduOperations {
-	public static final Log l = LogFactory.getLog(KomaduOperations.class);
-	public static final String  ERROR_STRING = "SERVER ERROR";
+    public static final Log l = LogFactory.getLog(KomaduOperations.class);
+    public static final String ERROR_STRING = "SERVER ERROR";
 
-	public static void storeNotification(NotificationIngester ingester, NotificationSummary.NotificationTypeEnum notificationType, String notification){
-		
-		try {
-			switch(notificationType){
-				case ADD_ACTIVITY_ACTIVITY_RELATION:
-					ingester.ingestActivityActivityRelationship(AddActivityActivityRelationshipDocument.Factory.parse(notification));
-					return;
-				case ADD_AGENT_ACTIVITY_RELATION:
-					ingester.ingestAgentActivityRelationship(AddAgentActivityRelationshipDocument.Factory.parse(notification));
-					return;
-				case ADD_AGENT_AGENT_RELATION:
-					ingester.ingestAgentAgentRelationship(AddAgentAgentRelationshipDocument.Factory.parse(notification));
-					return;
-				case ADD_ENTITY_ENTITY_RELATION:
-					ingester.ingestEntityEntityRelationship(AddEntityEntityRelationshipDocument.Factory.parse(notification));
-					return;
-				case ADD_ACTIVITY_ENTITY_RELATION:
-					ingester.ingestActivityEntityRelationship(AddActivityEntityRelationshipDocument.Factory.parse(notification));
-					return;
-				case ADD_AGENT_ENTITY_RELATION:
-					ingester.ingestAgentEntityRelationship(AddAgentEntityRelationshipDocument.Factory.parse(notification));
-					return;
-				case ADD_ATTRIBUTES:
-					ingester.ingestAddAttributes(AddAttributesDocument.Factory.parse(notification));
-					return;
-				case UNKNOWN_TYPES:
-					return;
-				default:
-					return;
-		
-			}
-		} catch (IngestException e1) {
-			e1.printStackTrace();
-			l.error("Ingest Exception.");
-			l.error(e1.getStackTrace());
-		} catch (XmlException e) {
-			l.error("XML Exception.");
-			l.error(e.getStackTrace());
-		}
-	}
+    public static void storeNotification(NotificationIngester ingester, NotificationSummary.NotificationTypeEnum
+            notificationType, String notification) {
 
-	
-	public static String queryProvenance(QueryImplementer querier, XmlObject query){
-		try {
-			QueryTypeEnum QueryType= QueryTypeEnum.determineQueryTypeFromXmlBeansDocument(query);
-			switch(QueryType){
-			case FIND_SERVICE:
-                FindActivityResponseDocument findActivityResponseDocument = null;
+        try {
+            switch (notificationType) {
+                case ADD_ACTIVITY_ACTIVITY_RELATION:
+                    ingester.ingestActivityActivityRelationship(
+                            AddActivityActivityRelationshipDocument.Factory.parse(notification));
+                    return;
+                case ADD_AGENT_ACTIVITY_RELATION:
+                    ingester.ingestAgentActivityRelationship(
+                            AddAgentActivityRelationshipDocument.Factory.parse(notification));
+                    return;
+                case ADD_AGENT_AGENT_RELATION:
+                    ingester.ingestAgentAgentRelationship(
+                            AddAgentAgentRelationshipDocument.Factory.parse(notification));
+                    return;
+                case ADD_ENTITY_ENTITY_RELATION:
+                    ingester.ingestEntityEntityRelationship(
+                            AddEntityEntityRelationshipDocument.Factory.parse(notification));
+                    return;
+                case ADD_ACTIVITY_ENTITY_RELATION:
+                    ingester.ingestActivityEntityRelationship(
+                            AddActivityEntityRelationshipDocument.Factory.parse(notification));
+                    return;
+                case ADD_AGENT_ENTITY_RELATION:
+                    ingester.ingestAgentEntityRelationship(
+                            AddAgentEntityRelationshipDocument.Factory.parse(notification));
+                    return;
+                case ADD_ATTRIBUTES:
+                    ingester.ingestAddAttributes(
+                            AddAttributesDocument.Factory.parse(notification));
+                    return;
+                case UNKNOWN_TYPES:
+                    return;
+                default:
+            }
+        } catch (IngestException e1) {
+            e1.printStackTrace();
+            l.error("Ingest Exception.");
+            l.error(e1.getStackTrace());
+        } catch (XmlException e) {
+            l.error("XML Exception.");
+            l.error(e.getStackTrace());
+        }
+    }
 
-				try {
-					findActivityResponseDocument = querier.findActivity((FindActivityRequestDocument) query);
-				} catch (QueryException qe) {
-					l.error("Error in findServiceRequest().");
-					l.error(qe.getMessage());
-					return ERROR_STRING + ": " + qe.getMessage();
-				}
-
-				return  findActivityResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
-
-			case GET_CONTEXT_WORKFLOW_GRAPH:
-				GetContextWorkflowGraphResponseDocument getContextWorkflowGraphResponseDocument  = null;
-
-				try {
-					getContextWorkflowGraphResponseDocument  = querier.getContextWorkflowGraph((GetContextWorkflowGraphRequestDocument)query);
-				} catch (QueryException qe) {	
-					l.error("Error in findAbstractDataProduct().");
-					l.error(qe.getMessage());					
-					return ERROR_STRING + ": " + qe.getMessage();
-				}
-
-				return getContextWorkflowGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
-			
-			case GET_ENTITY_GRAPH:
-				GetEntityGraphResponseDocument getEntityGraphResponseDocument = null;
-
-				try {
-					getEntityGraphResponseDocument = querier.getEntityGraph((GetEntityGraphRequestDocument)query);
-				} catch (QueryException qe) {	
-					l.error("Error in findAbstractMethod().");
-					l.error(qe.getMessage());
-					return ERROR_STRING + ": " + qe.getMessage();
-				}
-				
-				return getEntityGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
-			
-			case GET_ACTIVITY_GRAPH:
-				GetActivityGraphResponseDocument getActivityGraphResponseDocument = null;
-
-				try {
-					getActivityGraphResponseDocument = querier.getActivityGraph((GetActivityGraphRequestDocument)query);
-				} catch (QueryException qe) {
-					l.error("Error in findAbstractService().");
-					l.error(qe.getMessage());
-					return ERROR_STRING + ": " + qe.getMessage();
-				}
-
-				return getActivityGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
-			
-			case GET_AGENT_GRAPH:
-				GetAgentGraphResponseDocument getAgentGraphResponseDocument = null;
-
-				try {
-					getAgentGraphResponseDocument = querier.getAgentGraph((GetAgentGraphRequestDocument)query);
-				} catch (QueryException qe) {	
-					l.error("Error in findDataProduct().");
-					l.error(qe.getMessage());
-					return ERROR_STRING + ": " + qe.getMessage();
-				}
-
-				return getAgentGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
-			
-			case UNKNOWN_TYPES:
-					l.error("Unknown query type.");					
-					return "Unknown Query";
-			default:
-					l.error("Unknown query type.");
-					return "Unknown Query";
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "[Exception in queryProvenance]" + e.getMessage();
-		}
-	}
+    public static String queryProvenance(QueryImplementer querier, XmlObject query) {
+        try {
+            QueryTypeEnum QueryType = QueryTypeEnum.determineQueryTypeFromXmlBeansDocument(query);
+            switch (QueryType) {
+                case FIND_ACTIVITY:
+                    FindActivityResponseDocument findActivityResponseDocument;
+                    try {
+                        findActivityResponseDocument = querier.findActivity((FindActivityRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findServiceRequest().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return findActivityResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case GET_ACTIVITY_DETAIL:
+                    GetActivityDetailResponseDocument getActivityDetailResponseDocument;
+                    try {
+                        getActivityDetailResponseDocument = querier.getActivityDetail((GetActivityDetailRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findServiceRequest().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return getActivityDetailResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case FIND_ENTITY:
+                    FindEntityResponseDocument findEntityResponseDocument;
+                    try {
+                        findEntityResponseDocument = querier.findEntity((FindEntityRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findServiceRequest().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return findEntityResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case GET_ENTITY_DETAIL:
+                    GetEntityDetailResponseDocument getEntityDetailResponseDocument;
+                    try {
+                        getEntityDetailResponseDocument = querier.getEntityDetail((GetEntityDetailRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findServiceRequest().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return getEntityDetailResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case GET_CONTEXT_WORKFLOW_GRAPH:
+                    GetContextWorkflowGraphResponseDocument getContextWorkflowGraphResponseDocument;
+                    try {
+                        getContextWorkflowGraphResponseDocument = querier.getContextWorkflowGraph((GetContextWorkflowGraphRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findAbstractDataProduct().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return getContextWorkflowGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case GET_ENTITY_GRAPH:
+                    GetEntityGraphResponseDocument getEntityGraphResponseDocument;
+                    try {
+                        getEntityGraphResponseDocument = querier.getEntityGraph((GetEntityGraphRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findAbstractMethod().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return getEntityGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case GET_ACTIVITY_GRAPH:
+                    GetActivityGraphResponseDocument getActivityGraphResponseDocument;
+                    try {
+                        getActivityGraphResponseDocument = querier.getActivityGraph((GetActivityGraphRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findAbstractService().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return getActivityGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case GET_AGENT_GRAPH:
+                    GetAgentGraphResponseDocument getAgentGraphResponseDocument;
+                    try {
+                        getAgentGraphResponseDocument = querier.getAgentGraph((GetAgentGraphRequestDocument) query);
+                    } catch (QueryException qe) {
+                        l.error("Error in findDataProduct().");
+                        l.error(qe.getMessage());
+                        return ERROR_STRING + ": " + qe.getMessage();
+                    }
+                    return getAgentGraphResponseDocument.xmlText(new XmlOptions().setSavePrettyPrint());
+                case UNKNOWN_TYPES:
+                    l.error("Unknown query type.");
+                    return "Unknown Query";
+                default:
+                    l.error("Unknown query type.");
+                    return "Unknown Query";
+            }
+        } catch (Exception e) {
+            l.error("Error while Querying.", e);
+            return "[Exception in queryProvenance]" + e.getMessage();
+        }
+    }
 }
--- service-core/src/main/java/edu/indiana/d2i/komadu/query/QuerySummary.java
+++ service-core/src/main/java/edu/indiana/d2i/komadu/query/QuerySummary.java
@@ -27,29 +27,7 @@
 package edu.indiana.d2i.komadu.query;
 
 import org.apache.xmlbeans.XmlObject;
-import org.openprovenance.prov.model.*;
-import org.openprovenance.prov.xml.Other;
-import org.w3.www.ns.prov.Document;
 
-import org.openprovenance.prov.model.*;
-import org.openprovenance.prov.model.ActedOnBehalfOf;
-import org.openprovenance.prov.model.Activity;
-import org.openprovenance.prov.model.Agent;
-import org.openprovenance.prov.model.AlternateOf;
-import org.openprovenance.prov.model.Entity;
-import org.openprovenance.prov.model.HadMember;
-import org.openprovenance.prov.model.SpecializationOf;
-import org.openprovenance.prov.model.Used;
-import org.openprovenance.prov.model.WasAssociatedWith;
-import org.openprovenance.prov.model.WasAttributedTo;
-import org.openprovenance.prov.model.WasDerivedFrom;
-import org.openprovenance.prov.model.WasEndedBy;
-import org.openprovenance.prov.model.WasGeneratedBy;
-import org.openprovenance.prov.model.WasInformedBy;
-import org.openprovenance.prov.model.WasInvalidatedBy;
-import org.openprovenance.prov.model.WasStartedBy;
-import org.openprovenance.prov.xml.*;
-import org.w3.www.ns.prov.Document;
 /**
  * @author Quan Zhou(quzhou@indiana.edu)
  * 
@@ -60,28 +38,11 @@ public class QuerySummary {
 	public static final QuerySummary UNKNOWN_QUERY_SUMMARY = new QuerySummary(
 			QueryTypeEnum.UNKNOWN_TYPES);
 
-	/*  public FindActivityResponseDocument findActivity(
-            FindActivityRequestDocument findServiceRequest)
-            throws QueryException;
-
-    public GetContextWorkflowGraphResponseDocument getContextWorkflowGraph(
-            GetContextWorkflowGraphRequestDocument getWorkflowGraphRequest)
-            throws QueryException;
-    
-    public GetEntityGraphResponseDocument getEntityGraph(
-            GetEntityGraphRequestDocument getEntityGraphRequest) 
-            throws QueryException;
-
-    public GetActivityGraphResponseDocument getActivityGraph(
-            GetActivityGraphRequestDocument getActivityGraphRequest)
-            throws QueryException;
-
-    public GetAgentGraphResponseDocument getAgentGraph(
-            GetAgentGraphRequestDocument getAgentGraphRequest)
-            throws QueryException;*/
-	
 	public static enum QueryTypeEnum {
-		FIND_SERVICE, 
+        FIND_ACTIVITY, 
+        GET_ACTIVITY_DETAIL,
+        FIND_ENTITY,
+        GET_ENTITY_DETAIL,
 		GET_CONTEXT_WORKFLOW_GRAPH, 
 		GET_ENTITY_GRAPH, 
 		GET_ACTIVITY_GRAPH, 
@@ -91,7 +52,13 @@ public class QuerySummary {
 		public static QueryTypeEnum determineQueryTypeFromXmlBeansDocument(
 				XmlObject document) {
 			if (document instanceof  FindActivityRequestDocument) {
-				return FIND_SERVICE;
+				return FIND_ACTIVITY;
+            } else if (document instanceof GetActivityDetailRequestDocument) {
+                return GET_ACTIVITY_DETAIL;
+            } else if (document instanceof FindEntityRequestDocument) {
+                return FIND_ENTITY;
+            } else if (document instanceof GetEntityDetailRequestDocument) {
+                return GET_ENTITY_DETAIL;
 			} else if (document instanceof GetContextWorkflowGraphRequestDocument) {
 				return GET_CONTEXT_WORKFLOW_GRAPH;
 			} else if (document instanceof GetEntityGraphRequestDocument) {
