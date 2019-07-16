c54becbc606c663759186ca4f2ef8a3e9adc6408
swsachith <swsachith@gmail.com>
2019-07-16 14:03:59 -0400

    taking the workflow_name as an argument to the processor

--- GrayScott/komadu_client/main.py
+++ GrayScott/komadu_client/main.py
@@ -3,13 +3,14 @@
 A standalone Komadu connector that sends the notifications to Komadu via filesystem reading.
 
 Usage:
-  komaduConnect [--polling | --static] <location>
+  komaduConnect [--polling | --static] --workflow_type=<workflow_name> <location>
 
 Options:
   -h --help     Show this screen.
   --polling     Start the connector in the polling mode (looks for file creations in real-time)
   --static      Start the connector in static mode (reads the current files and processes them)
-  <location>    Directory of which to observe
+  --workflow_type Type of the workflow (ex: gray-scott)
+  <location>    Directory of which to extract the data from
 """
 import sys
 import os
@@ -50,13 +51,17 @@ class EventProcessor(threading.Thread):
     ex: Grayscott events are processed by GrayScott event processor
     """
 
-    def __init__(self, event_queue):
+    def __init__(self, event_queue, workflow_name):
         super(EventProcessor, self).__init__()
         self.event_queue = event_queue
         logger.info("Event processor initialized!")
+        self.workflow_name = workflow_name
         self.komadu_conn = KomaduClient()
-        self.grayscott_processor = GrayScottEventProcessor(self.komadu_conn)
-        self.brusselator_processor = BrusselatorEventProcessor(self.komadu_conn)
+        # setting up the correct workflow processor
+        if GRAYSCOTT_WORKFLOW in self.workflow_name:
+            self.processor = GrayScottEventProcessor(self.komadu_conn)
+        elif BRUSSELATOR_WORKFLOW_NAME in self.workflow_name:
+            self.processor = BrusselatorEventProcessor(self.komadu_conn)
         return
 
     def run(self):
@@ -70,7 +75,6 @@ class EventProcessor(threading.Thread):
         filename = event_content[-1]
         file_extension = os.path.splitext(filename)[1]
         username = event_content[4]
-        workflow_type = event_content[3]
 
         if str(filename).startswith("."):
             # ignore files starting with a '.'
@@ -80,7 +84,7 @@ class EventProcessor(threading.Thread):
         username = "swithana"
         location = "summit"
         log_event = "Processing file: {} filepath: {} workflow_type {}"
-        logger.debug(log_event.format(filename, file_path, workflow_type))
+        logger.debug(log_event.format(filename, file_path, self.workflow_name))
 
         if FOBS_FILE in file_path:
             logger.info("Processing the fobs file: {}".format(file_path))
@@ -90,14 +94,10 @@ class EventProcessor(threading.Thread):
                 'utf-8').replace('"', "'")
             self.komadu_conn.publish_data(activity_activity)
 
-        elif GRAYSCOTT_WORKFLOW in file_path:
-            self.grayscott_processor.process_event(username, filename, file_extension, file_path, location)
-
-        elif BRUSSELATOR_WORKFLOW_NAME in file_path:
-            self.brusselator_processor.process_event(username, filename, file_extension, file_path, location)
+        self.processor.process_event(username, filename, file_extension, file_path, location)
 
 
-def process_polling(path):
+def process_polling(path, workflow_name):
     data_queue = queue.Queue()
     event_handler = ExperimentEventHandler(data_queue)
 
@@ -106,7 +106,7 @@ def process_polling(path):
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
 
-    event_processor = EventProcessor(data_queue)
+    event_processor = EventProcessor(data_queue, workflow_name)
     event_processor.start()
     try:
         while True:
@@ -116,18 +116,31 @@ def process_polling(path):
     observer.join()
 
 
-def process_static(path):
+def process_static(path, workflow_name):
     data_queue = queue.Queue()
-    event_processor = EventProcessor(data_queue)
+    event_processor = EventProcessor(data_queue, workflow_name)
     files = glob.glob(path + os.sep + "**", recursive=True)
+    #
+    # fob_json_list = []
+    # for i in range(len(files)):
+    #     if FOBS_FILE in files[i]:
+    #         fob_json_list.append(files[i])
+    #
+    # if len(fob_json_list):
+    #     for fob_file in fob_json_list:
+    #         print("Processing... {}".format(fob_file))
+    #         files.remove(fob_file)
+    #         files.insert(0, fob_file)
+    #
+    # print(files)
     for file in files:
         event_processor.process_file_event(file)
 
 
 if __name__ == "__main__":
     arguments = docopt(__doc__, version="1.0")
-
+    print(arguments)
     if not arguments['--polling']:
-        process_static(arguments["<location>"])
+        process_static(arguments["<location>"], arguments["--workflow_type"])
     else:
-        process_polling(arguments["<location>"])
+        process_polling(arguments["<location>"], arguments["--workflow_type"])
