0ff3f3d61849522433ef47dccc023049c6305b94
swsachith <swsachith@gmail.com>
2019-07-18 11:29:41 -0400

    fixing tau key extraction error with Komadu

--- GrayScott/komadu_client/test/test_tau_extractor.py
+++ GrayScott/komadu_client/test/test_tau_extractor.py
@@ -1,10 +1,12 @@
 import unittest
 from komadu_client.parsers.tau_profile_parser import parse_tau_file
 
+
 class TestTauParser(unittest.TestCase):
 
     def test_parse_bp_file(self):
         filename = "./resources/gray-scott/tau-metrics.bp"
         attributes = parse_tau_file(filename)
         actual = attributes.toxml("utf-8", element_name='ns1:attributes').decode('utf-8').replace('"', "'")
-        print(actual)
+        expected = "<?xml version='1.0' encoding='utf-8'?><ns1:attributes xmlns:ns1='http://komadu.d2i.indiana.edu'><ns1:attribute><ns1:property>hostname</ns1:property><ns1:value>swithana-mbp.local</ns1:value></ns1:attribute><ns1:attribute><ns1:property>counter_1</ns1:property><ns1:value>Message size sent to all nodes</ns1:value></ns1:attribute></ns1:attributes>"
+        self.assertEqual(actual, expected)
